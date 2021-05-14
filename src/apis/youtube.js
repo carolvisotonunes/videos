79271c5b4b6841bbaf4dd395af05e193ba189348
Caroline Visoto Nunes <carolvisoto@gmail.com>
2021-05-14 17:23:59 +1000

    search vedeo abd add video type

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
 const KEY= 'AIzaSyBgGvDS0UC7jwc0-jYqjAThX4Uo5wi1EkI';
+
+export default axios.create({
+    baseURL:'https://www.googleapis.com/youtube/v3';
+    params:{
+        part:'snippet',
+        type:'videos',
+        maxResults:5,
+        key: KEY
+    }
+});
